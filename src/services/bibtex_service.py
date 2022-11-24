fd5fd969048b130600922dfcb41d4a2625fa976a
PyryL <pyry@pyry.info>
2022-11-24 10:09:20 +0200

    unicode-nimien enkoodaus latex-muotoon

--- src/services/bibtex_service.py
+++ src/services/bibtex_service.py
@@ -1,12 +1,16 @@
 from bibtexparser.bwriter import BibTexWriter
 from bibtexparser.bibdatabase import BibDatabase
+from pylatexenc.latexencode import UnicodeToLatexEncoder
 
 class BibtexService:
     def __init__(self):
         self.writer = BibTexWriter()
         self.db = BibDatabase()
+        self.encoder = UnicodeToLatexEncoder(replacement_latex_protection='braces-all')
 
     def uusi_viite(self, viite):
+        for key in viite:
+            viite[key] = self.encoder.unicode_to_latex(viite[key])
         self.db.entries.append(viite)
 
     def vie_viitteet(self, tiedostonimi):
