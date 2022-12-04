28163ca137952b8cb0a83fc4941f90e12a139d12
zmejka <evgeniia.minaeva@tutanota.com>
2022-12-04 17:56:20 +0200

    poistettu käyttämätön testi

--- src/entities/book.py
+++ src/entities/book.py
@@ -12,9 +12,6 @@ class Book(Reference):
     def set_isbn(self, isbn):
         self._isbn = isbn
 
-    def __str__(self):
-        return f"{self._author:30} {self._title:40} {self._year:4}"
-
     def generate_id(self):
         surname = self._author.split(",")
         return surname[0]+str(self._year)
--- src/tests/book_test.py
+++ src/tests/book_test.py
@@ -21,11 +21,6 @@ class TestBook(unittest.TestCase):
         generated_id = self.book.generate_id()
         self.assertEqual(generated_id, 'Kurose2019')
 
-    def test_column_printing(self):
-        text = str(self.book)
-        expected = 'Kurose, Jim; Ross, Keith       Computer Networking                      2019'
-        self.assertEqual(text, expected)
-
     def test_command_vie_creates_bib_file(self):
         self.reference_manager.vie_viitteet_tiedostoon('viitteet_test')
         self.assertTrue(os.path.exists('viitteet_test.bib'))
