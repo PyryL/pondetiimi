5b8db142b91030e00e93363cf7419d237831a2e6
Topias Ukkonen <ukkonentopias@gmail.com>
2022-12-09 20:02:32 +0200

    testin feilaus korjattu

--- src/services/input_validation.py
+++ src/services/input_validation.py
@@ -107,7 +107,7 @@ class InputValidation:
         return True
 
     @classmethod
-    def error_message(cls, error_type:str) -> str:
+    def error_message(cls, error_type:str="tyhja") -> str:
         '''
         Palauttaa virheilmoituksen annetun virhetypin mukaan.
         Args:
