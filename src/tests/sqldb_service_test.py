3b588af72c30d3edce89aa7a6508fb1a7ea1017c
Ma <asasf@sfsfsdf.com>
2022-12-15 19:24:34 +0200

    Lisätty testejä.

--- src/tests/sqldb_service_test.py
+++ src/tests/sqldb_service_test.py
@@ -1,4 +1,7 @@
-class Testclass SqldbService(unittest.TestCase):
+import unittest
+from services.sqldb_service import SqldbService
+
+class TestSqldbService(unittest.TestCase):
     '''Tietokannan testit'''
 
     def setUp(self):
